253af4e43f458300aebd67e615c47520580edb41
yasmim :) <108752556+yasmimNamie@users.noreply.github.com>
2024-05-09 16:21:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const dataAtual = new Date();
-let dataObjetivo = prompt("Data final (formatoANO-MÊS-DIA, exemplo: 2024-05-30");
+let dataObjetivo = prompt("Data final (formato ANO-MÊS-DIA, exemplo: 2024-05-30");
 dataObjetivo = new Date(dataObjetivo+"t23:59:59");
 let diasQueFaltam = Math.floor((dataObjetivo - dataAtual) / 86400000);
-document.querySelector("#dias-restantes").textContent = diasQueFaltam+" dias";
+document.querySelector("#dias_restantes").textContent = diasQueFaltam+" dias";
